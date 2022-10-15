5f48c55337d9bcd1a0c54795eeba153d90bc8e7e
GabrielDSanta <eloviveiros@hotmail.com>
2022-10-15 13:30:18 -0300

    Funções básicas do jogo finalizadas

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 "use strict";
+let Torres = [];
+let CampoDeBatalha = [];
+let Inimigos = [];
+let Continuar = true;
+Torres.length = 9;
+CampoDeBatalha.length = 9;
 class Torre {
     constructor(nome, ataque, alcance, nivel, valor) {
         this.SetName(nome);
@@ -59,9 +65,60 @@ class Inimigo {
         this.SetVida(this.GetVida() - dano);
     }
 }
-let Torres = [];
-let Inimigos = [];
-let Continuar = true;
 while (Continuar) {
     let Pergunta = prompt("1 Começar Jogo /// 2 Criar Torre /// 3 Criar Inimigo /// 4 Finalizar");
+    switch (Pergunta) {
+        case "1":
+            ComeçarPartida();
+            break;
+        case "2":
+            CriarTorre();
+            break;
+        case "3":
+            CriarInimigo();
+            break;
+        case "4":
+            Continuar = false;
+            break;
+        default:
+            alert("Opção inválida");
+            break;
+    }
+}
+function CriarTorre() {
+    let Error = false;
+    let Nome = String(prompt("Qual o Nome da Torre ?"));
+    let Ataque = Number(prompt("Qual o Ataque da Torre ?"));
+    let Alcance = Number(prompt("Qual o Alcance da Torre ?"));
+    let Nivel = Number(prompt("Qual o Nivel da Torre ?"));
+    let Valor = Number(prompt("Qual o Valor da Torre ?"));
+    let NewTorre = new Torre(Nome, Ataque, Alcance, Nivel, Valor);
+    let IndexTorre = Number(prompt("Torre criada ! Selecione a posição dela no tabuleiro (0 ~ 9)"));
+    if (IndexTorre > 9 || IndexTorre < 0) {
+        return alert("Posição inválida");
+    }
+    Torres.forEach(VerificateIndex);
+    function VerificateIndex(item, index) {
+        if (Torres[IndexTorre] !== undefined) {
+            Error = true;
+            alert("Você já possui uma Torre nessa posição");
+        }
+    }
+    if (Error == false) {
+        Torres.splice(IndexTorre, 1, NewTorre);
+    }
+}
+function CriarInimigo() {
+    let Nome = String(prompt("Qual o Nome do Inimigo ?"));
+    let Vida = Number(prompt("Qual a Vida do Inimigo ?"));
+    let NewInimigo = new Inimigo(Nome, Vida);
+    let QuantidadeInimigos = Number(prompt("Quantos inimigos vão ter na Partida ? (Max 9)"));
+    if (QuantidadeInimigos > 9) {
+        return alert("Valor inválido");
+    }
+    for (let index = 0; index < QuantidadeInimigos; index++) {
+        Inimigos.unshift(NewInimigo);
+    }
+}
+function ComeçarPartida() {
 }
--- script.ts
+++ script.ts
@@ -1,6 +1,16 @@
 
 
 
+let Torres: object[] = []
+let CampoDeBatalha: object[] = []
+let Inimigos: object[] = []
+let Continuar = true
+
+Torres.length = 9
+CampoDeBatalha.length = 9
+
+
+
 
 class Torre{
     private _Nome: string
@@ -49,7 +59,6 @@ class Torre{
         return this._Valor
     }
 }
-
 class Inimigo{
     private _Nome: string
     private _Vida: number
@@ -79,11 +88,81 @@ class Inimigo{
 }
 
 
-let Torres: object[] = []
-let Inimigos: object[] = []
-
-let Continuar = true
-
 while(Continuar){
     let Pergunta = prompt("1 Começar Jogo /// 2 Criar Torre /// 3 Criar Inimigo /// 4 Finalizar")
+    switch(Pergunta){
+
+        case "1":
+        ComeçarPartida()
+        break;
+
+
+        case "2":
+        CriarTorre()
+        break;
+
+        case "3":
+        CriarInimigo()
+        break;
+
+        case "4":
+        Continuar = false
+        break;
+
+        default:
+        alert("Opção inválida")
+        break;
+    }
+}
+
+
+function CriarTorre(){
+    let Error = false
+
+    let Nome = String(prompt("Qual o Nome da Torre ?"))
+    let Ataque = Number(prompt("Qual o Ataque da Torre ?"))
+    let Alcance = Number(prompt("Qual o Alcance da Torre ?"))
+    let Nivel = Number(prompt("Qual o Nivel da Torre ?"))
+    let Valor = Number(prompt("Qual o Valor da Torre ?"))
+
+    let NewTorre = new Torre(Nome, Ataque, Alcance, Nivel, Valor)
+    let IndexTorre = Number(prompt("Torre criada ! Selecione a posição dela no tabuleiro (0 ~ 9)"))
+    if(IndexTorre > 9 || IndexTorre < 0){
+        return alert("Posição inválida")
+    }
+
+    Torres.forEach(VerificateIndex)
+
+    function VerificateIndex(item: object, index: number){
+        if(Torres[IndexTorre] !== undefined){
+            Error = true
+            alert("Você já possui uma Torre nessa posição")
+        }
+    }
+
+    if(Error == false){
+        Torres.splice(IndexTorre, 1, NewTorre)
+    }
+
+
+}
+
+function CriarInimigo(){
+    let Nome = String(prompt("Qual o Nome do Inimigo ?"))
+    let Vida = Number(prompt("Qual a Vida do Inimigo ?"))
+
+    let NewInimigo = new Inimigo(Nome, Vida)
+
+    let QuantidadeInimigos = Number(prompt("Quantos inimigos vão ter na Partida ? (Max 9)"))
+    if(QuantidadeInimigos > 9){
+        return alert("Valor inválido")
+    }
+
+    for(let index = 0; index < QuantidadeInimigos; index++){
+        Inimigos.unshift(NewInimigo)
+    }
+}
+
+function ComeçarPartida(){
+
 }
